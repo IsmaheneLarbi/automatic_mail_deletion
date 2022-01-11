d3089cbe911238fb53dbff5112eac0d4749f82c7
IsmaheneLarbi <meriemismahlarbi@gmail.com>
2022-01-11 19:28:34 +0100

    sender + since

---
+++ automate_mail_deletion.py
@@ -0,0 +1,16 @@
+import imaplib
+import time
+
+provider = "gmail"
+username = f"youraddress@{provider}.com"
+password = "your password"
+imap = imaplib.IMAP4_SSL(f"imap.{provider}.com")
+imap.login(username, password)
+imap.select('"INBOX"')
+imap.search(None, "FROM 'info@meetup.com' SINCE '02-Jan-2022'")
+mails = mails[0].split(b' ')
+for mail in mails:
+    imap.store(mail, '+FLAGS', '\\Deleted')
+imap.expunge()
+imap.close()
+imap.logout()
