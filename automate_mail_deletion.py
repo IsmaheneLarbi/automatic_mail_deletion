db9b3c822e26a7cd9c7ac3c520248d5596403ee5
IsmaheneLarbi <meriemismahlarbi@gmail.com>
2022-01-14 20:53:43 +0100

    delete emails received in the last day

--- automate_mail_deletion.py
+++ automate_mail_deletion.py
@@ -1,12 +1,18 @@
 import imaplib
 
+
+from datetime import datetime, timedelta
+
 provider = "gmail"
-username = f"youraddress@{provider}.com"
-password = "your password"
+username = f"yourusername@{provider}.com"
+password = "your password for this app"
 imap = imaplib.IMAP4_SSL(f"imap.{provider}.com")
 imap.login(username, password)
 imap.select('"INBOX"')
-status, mails = imap.search(None, 'FROM "info@meetup.com" SINCE "02-Jan-2022"')
+today = datetime.today()
+yesterday = today - timedelta(days=1)
+yesterday = yesterday.strftime("%d-%b-%Y")
+status, mails = imap.search(None, f'FROM "info@meetup.com" SINCE "{yesterday}"')
 mails = mails[0].split(b' ')
 for mail in mails:
     imap.store(mail, '+FLAGS', '\\Deleted')
